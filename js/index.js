b050af3d3cf487d13246999784fe34b5a26ad8a5
Adam Urbina <adamurbina@gmail.com>
2019-02-17 20:03:09 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  var username = document.getElementById("username");
+  req.open('GET', 'https://api.github.com/users/' + username.value + '/repos');
+  req.send();
+}
