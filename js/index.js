2892b8c6036be831bc5d3a930d085135c1e64ba2
Adam Urbina <adamurbina@gmail.com>
2019-02-17 20:24:34 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function showRepositories(){
   var repos = JSON.parse(this.responseText);
   const repoList = `${repos.map(
     repo =>
-      '<a href=' + repo.owner['url'] +'>'+ repo.name +'</a><br>'
+      '<a href=' + repo.url +'>'+ repo.name +'</a><br>'
   ).join('')}`;
   document.getElementById("repositories").innerHTML = repoList;
 }
